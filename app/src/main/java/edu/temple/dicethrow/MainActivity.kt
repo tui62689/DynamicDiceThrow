03ed85d92d1943e3e993265b353d0543dc06c449
tui62689 <112034701+tui62689@users.noreply.github.com>
2024-10-28 16:26:03 -0400

    changes

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -1,10 +1,12 @@
 package edu.temple.dicethrow
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
 
 
 /*
@@ -19,6 +21,8 @@ The Activity layout files for both Portrait and Landscape are already provided
 */
 
 class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
+
+    var containers = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,12 +31,31 @@ class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
             - Show only Button Fragment if portrait
             - show both fragments if Landscape
           */
+
+        containers = findViewById<View>(R.id.container2) != null
+        val buttonFragment = ButtonFragment()
+        val dieViewModel = ViewModelProvider(this)[DieViewModel::class.java]
+
+        if(containers == null){
+            supportFragmentManager.beginTransaction().add(R.id.container1, buttonFragment)
+                .addToBackStack(null).setReorderingAllowed(true).commit()
+        }
+        if(containers){
+            supportFragmentManager.beginTransaction().add(R.id.container2, DieFragment())
+                .commit()
+        }
+
+
     }
 
     /* TODO 2: switch fragments if portrait (no need to switch fragments if Landscape)
         */
     // Remember to place Fragment transactions on BackStack so then can be reversed
     override fun buttonClicked() {
+        if(containers){
+            supportFragmentManager.beginTransaction().replace(R.id.container1, DieFragment())
+                .setReorderingAllowed(true).addToBackStack(null).commit()
+        }
 
     }
 
